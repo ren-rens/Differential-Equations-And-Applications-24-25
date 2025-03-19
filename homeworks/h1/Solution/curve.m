4e0ef2fbbdf778172fa8ec7fb41b232889b201c9
Irena Stancheva <147410058+ren-rens@users.noreply.github.com>
2025-03-19 19:00:31 +0200

    Update curve.m

--- homeworks/h1/Solution/curve.m
+++ homeworks/h1/Solution/curve.m
@@ -6,14 +6,13 @@ grid on;
 
 xlabel('x');
 ylabel('y');
-title('Интегрална крива на задачата на Коши')
+title('Интегрална крива на задачата на Коши');
 
 x0 = 1; 
 y0 = 4;
 syms y(x);
 
-sol = dsolve(x * diff(y, x) == (12 * y^2 + 12 * x * y + 6 * x^2) / (8 * y + 6 * x), ...
-y(x0) == y0);
+sol = dsolve(x * diff(y, x) == (12 * y^2 + 12 * x * y + 6 * x^2) / (8 * y + 6 * x), y(x0) == y0);
 
 xvalid = (15 / 4) / (19^2 ++ 15 / 4) + 1;
 x = xvalid:0.01:10;
